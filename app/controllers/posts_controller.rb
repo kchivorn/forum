b802d58f47999f406c32bf57ff1266b2f6144d4d
kchivorn <kouch.chivorn@gmail.com>
2017-11-07 16:21:03 +0700

    Add post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,50 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @posts = Post.all.order(created_at: :desc)
+  end
 
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "The post was created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to @post, notice: "The post was updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
