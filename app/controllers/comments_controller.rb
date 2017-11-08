4545362942d04d9594954b342d84c5eab50252d4
kchivorn <kouch.chivorn@gmail.com>
2017-11-08 11:34:32 +0700

    Add comments to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, except: [:create]
+  before_action :authenticate_user!
 
   def create
     @comment = @post.comments.create(comment_params)
@@ -11,10 +13,31 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @comment.update_attributes(comment_params)
+      redirect_to @post, notice: "Comment was updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @post
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def comment_params
